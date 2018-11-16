66f75d76aee1fd3c89a5aeaf5388062225670195
Oliver Hunt <oliver@apple.com>
2018-11-16 10:32:15 -0800

    A few changes

--- tests/obj.rs
+++ tests/obj.rs
@@ -8,12 +8,14 @@
 
 extern crate obj;
 
+use obj::*;
 use std::fs::File;
 use std::io::BufReader;
-use obj::*;
 
 macro_rules! fixture {
-    ($name:expr) => (BufReader::new(File::open(concat!("tests/fixtures/", $name)).unwrap()))
+    ($name:expr) => {
+        BufReader::new(File::open(concat!("tests/fixtures/", $name)).unwrap())
+    };
 }
 
 #[test]
@@ -24,15 +26,27 @@ fn normal_cone() {
         (($($p:expr),*), ($($n:expr),*)) => ({
             Vertex {
                 position: [$(stringify!($p).parse::<f32>().unwrap()),*],
-                normal: [$(stringify!($n).parse::<f32>().unwrap()),*],
+                normal: Some([$(stringify!($n).parse::<f32>().unwrap()),*]),
             }
         })
     }
 
     assert_eq!(obj.name, Some("Cone".to_string()));
     assert_eq!(obj.vertices.len(), 96);
-    assert_eq!(obj.vertices[0], v!((-0.382682, -1.000000, -0.923880), (-0.259887, 0.445488, -0.856737)));
-    assert_eq!(obj.vertices[1], v!(( 0.000000,  1.000000,  0.000000), (-0.259887, 0.445488, -0.856737)));
+    assert_eq!(
+        obj.vertices[0],
+        v!(
+            (-0.382682, -1.000000, -0.923880),
+            (-0.259887, 0.445488, -0.856737)
+        )
+    );
+    assert_eq!(
+        obj.vertices[1],
+        v!(
+            (0.000000, 1.000000, 0.000000),
+            (-0.259887, 0.445488, -0.856737)
+        )
+    );
     assert_eq!(obj.indices.len(), 96);
     assert_eq!(obj.indices[0], 0);
     assert_eq!(obj.indices[1], 1);
